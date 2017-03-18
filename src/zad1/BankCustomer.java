734f9b4484528c5ec3a41b13902b9a59990a23ee
Marek <mpietrewicz@gmail.com>
2017-03-18 13:31:06 +0100

    Poprawa w BankCustomer

--- src/zad1/BankCustomer.java
+++ src/zad1/BankCustomer.java
@@ -5,18 +5,21 @@ package zad1;
  */
 public class BankCustomer {
 
-    private String name;
-    private Account konto;
+//    private String name; // do usunięcie - nie wykorzystujesz
+    public Account konto;
+    public Person klient;
 
     BankCustomer (Person klient) {
-        this.name = klient.name;
+//        this.name = klient.name; // trochę to bez sensu po co powielać imie klienta?
+        this.klient = klient; // lepiej zapisać obiekt klient ;)
+        this.konto = new Account(); // tworząc klienta zakladamy od razu jemu konto
     }
 
     public Account getAccount() {
-        return konto;
+        return konto; // zwracasz coś pustego - coś co zadeklarowałaś ale go nie wypełniłaś
     }
 
     void show () {
-        System.out.println("Klient: " + name + " stan konta " + " " + konto.stan() + ".");
+        System.out.println("Klient: " + klient.name + " stan konta " + " " + konto.stan() + ".");
     }
 }
